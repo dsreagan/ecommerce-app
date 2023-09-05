a875a9a505c1c7baff843df02c42394a7ce784c1
Daniel Reagan <d.reagan.dr@gmail.com>
2023-09-05 15:59:02 -0500

    Add payment functionality

--- api/index.js
+++ api/index.js
@@ -18,13 +18,11 @@ mongoose
 
 app.use(cors())
 app.use(express.json())
-app.use("/api/users", userRoute)
 app.use("/api/auth", authRoute)
+app.use("/api/users", userRoute)
 app.use("/api/products", productRoute)
-app.use("/api/cart", cartRoute)
+app.use("/api/carts", cartRoute)
 app.use("/api/orders", orderRoute)
-
-
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Server running.")
