9942ad320898a045aa0cd15adca3fcde501f99cf
Daniel Reagan <d.reagan.dr@gmail.com>
2023-09-05 13:30:20 -0500

    Create auth routes

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 const cors = require("cors")
 const userRoute = require("./routes/user")
+const authRoute = require("./routes/auth")
 
 dotenv.config()
 
@@ -15,6 +16,7 @@ mongoose
 app.use(cors())
 app.use(express.json())
 app.use("/api/user", userRoute)
+app.use("/api/auth", authRoute)
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Server running.")
---
+++ api/routes/auth.js
@@ -0,0 +1,54 @@
+const router = require("express").Router()
+const CryptoJS = require("crypto-js")
+const jwt = require("jsonwebtoken")
+const User = require("../models/User")
+
+// Registration
+router.post("/register", async (req, res) => {
+  // error handle by checking if these fields exist and send a response if not
+
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASSWORD_KEY
+    ).toString(),
+  })
+
+  try {
+    const savedUser = await newUser.save()
+    res.status(201).json(savedUser)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+// Login
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username })
+    !user && res.status(401).json("Wrong username.")
+    const hashedPW = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASSWORD_KEY
+    )
+    const storedPassword = hashedPW.toString(CryptoJS.enc.Utf8)
+    req.body.password !== storedPassword &&
+      res.status(401).json("Wrong Password.")
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_KEY,
+      { expiresIn: "3d" }
+    )
+    const { password, ...userInfo } = user._doc
+    res.status(200).json({ ...userInfo, accessToken })
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+module.exports = router
