be38bf90cbfdcd106e02cd3f370875dcace3ed04
Daniel Reagan <d.reagan.dr@gmail.com>
2023-09-05 12:41:22 -0500

    Build models

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
+const cors = require("cors")
+const userRoute = require("./routes/user")
 
 dotenv.config()
 
@@ -9,6 +11,10 @@ mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("DB Connection Successful."))
   .catch((err) => console.log(err))
+
+app.use(cors())
+app.use(express.json())
+app.use("/api/user", userRoute)
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Server running.")
