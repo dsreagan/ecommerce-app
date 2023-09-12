d17fded3e3214f54dc974e3c165e9a56fcbe2ba9
Daniel Reagan <d.reagan.dr@gmail.com>
2023-09-12 12:50:05 -0500

    Promotional banner

--- client/src/components/PromotionBanner.tsx
+++ client/src/components/PromotionBanner.tsx
@@ -1,9 +1,12 @@
+import { Box, Text } from "@chakra-ui/react"
 
 const PromotionBanner = () => {
   return (
-    <div>
-      
-    </div>
+    <Box paddingX={3} bg="green.400">
+      <Text fontSize="md">
+        Sign in for a <Text fontSize="lg" fontWeight="bold" as="span">20% Discount!!!</Text>
+      </Text>
+    </Box>
   )
 }
 
--- client/src/pages/Layout.tsx
+++ client/src/pages/Layout.tsx
@@ -3,9 +3,12 @@ import { Outlet } from "react-router-dom"
 import AuthModal from "../components/AuthModal"
 import CartDrawer from "../components/CartDrawer"
 import Navbar from "../components/Navbar"
+import PromotionBanner from "../components/PromotionBanner"
 import WarningBanner from "../components/WarningBanner"
+import useLamaStore from "../store"
 
 export default function Layout() {
+  const user = useLamaStore((s) => s.user)
   const {
     isOpen: isAuthModelOpen,
     onClose: closeAuthModal,
@@ -22,6 +25,7 @@ export default function Layout() {
     <>
       <Box w="100%" pos="fixed" top={0} zIndex={10}>
         {/* <WarningBanner /> */}
+        {!user?.username && <PromotionBanner />}
         <Navbar openAuthModal={openAuthModal} openCartDrawer={openCartDrawer} />
       </Box>
       <Box paddingY={16}>
